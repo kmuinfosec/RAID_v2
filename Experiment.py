7247331915caaf08a050a74b9ff30de392fdd71e
pbkpch3514 <pbkpch@kookmin.ac.kr>
2022-06-20 18:08:05 +0900

    modify : hide sensitive path

--- Experiment.py
+++ Experiment.py
@@ -5,27 +5,7 @@ from Utils import *
 
 
 def experiment():
-
-    # pcap_path = r"C:\Users\user\spectator05\RAID_IoT-DDoS\data\3번째 데이터셋_2차익명화처리"
-    # pcap_dir = os.listdir(pcap_path)
-    # main([  'secret moon is best',
-    #         [os.path.join(pcap_path, dir) for dir in pcap_dir],
-    #         r"C:\Users\user\spectator05\RAID_IoT-DDoS\result",
-    #         "3rd_dataset_0.8",
-    #         0.8
-    #     ])
-    #example
-    pcap_path = rf"C:\Users\seclab\Downloads\6th_dataset\Possible_CVE_20220613\Elastic_Kibana_Timelion_Prototype_Pollution_Remote_Code_Execution_CVE-2019-7609"
-    pcap_dir = os.listdir(pcap_path)
-    main([  'secret moon is best',
-            [os.path.join(pcap_path, dir) for dir in pcap_dir],
-            os.getcwd(),
-            get_dir(os.getcwd()),
-            0.6,
-            False,
-            False
-        ])
-
+    pass
 
 if __name__ == "__main__":
     experiment()

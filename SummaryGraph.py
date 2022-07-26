0eec7eb0b6e993f3cc2dd4e201eb2b5456b4ba22
Woohyuk Jang <spectat@kookmin.ac.kr>
2022-07-26 22:50:47 +0900

    update : group graph, fix : cluster graph

--- SummaryGraph.py
+++ SummaryGraph.py
@@ -6,119 +6,92 @@ import os
 
 def SummaryGraph(result_path):
     main_df = pd.read_csv(os.path.join(result_path, "group_signatures.csv"))
-    if len(main_df["cluster_key_card"].unique()) == 0:
-        main_df["cluster_key_card"] = 1
-    group_key_df = main_df[["group", "key_card"]]
+    # if len(main_df["cluster_key_card"].unique()) == 0:
+    #     main_df["cluster_key_card"] = 1
+    group_key_df = main_df[['group', 'key_card', 'group_packet', 'biggest_cluster', 'cluster_packet', 'occurrence of most frequent signature']]
     group_key_df = group_key_df.drop_duplicates()
     group_key_df = group_key_df.sort_values("key_card", ascending=False)
 
     x = []
     packet = []
-    key_card = []
-    cluster_count = []
-    cluster_packets = []
+    bigcluster_packets = []
+    most_occurrence_signature = []
 
-    for i in group_key_df["group"].tolist():
-        temp_df = main_df[main_df["group"] == i]
-        temp = temp_df.values.tolist()
-        x.append(temp[0][0])
-        packet.append(temp[0][2])
-        key_card.append(temp[0][1])
-        if len(temp_df) == len(temp_df[temp_df["cluster"] != -1]):
-            cluster_count.append(len(temp_df))
+    for group in group_key_df['group'].tolist():
+        sub_df = group_key_df[group_key_df['group'] == group]
+        group_summary = sub_df.values.tolist()
+        x.append(group_summary[0][0])
+        packet.append(group_summary[0][2])
+        
+        if len(sub_df[sub_df['biggest_cluster'] != -1]) != 0:
+            bigcluster_packets.append(group_summary[0][4])
         else:
-            cluster_count.append(len(temp_df) - 1)
-        c_packets = []
-        if len(temp_df[temp_df["cluster"] == -1]) == 0:
-            c_packets.append(0)
-        else:
-            c_packets.append(
-                temp_df[temp_df["cluster"] == -1]["cluster_packet"].values[0]
-            )
-        if len(temp_df[temp_df["cluster"] != -1]) != 0:
-            c_packets.append(
-                sorted(
-                    temp_df[temp_df["cluster"] != -1]["cluster_packet"].tolist(),
-                    reverse=True,
-                )[0]
-            )
-        cluster_packets.append(c_packets)
+            bigcluster_packets.append(0)
+        most_occurrence_signature.append(group_summary[0][5])
 
-    bar_width = 0.3
-    alpha = 1
+    alpha = 0.7
+    bar_width = 0.2
+    fig, ax1 = plt.subplots(figsize=(20, 4))
 
-    # b0 = plt.bar(x2, value, color='g', width=bar_width, alpha=alpha, label='Packet Count')
-    # b3 = plt.bar(x2, value, color='none', width=bar_width, alpha=alpha, label='Remain', edgecolor=['r'])
-    # b4 = plt.bar(x2, value, color='b', width=bar_width, alpha=alpha, label='Opposite Cardinality')
+    index = range(0, len(x)*2, 2)
+    index2 = np.arange(-0.3, len(x)*2 - 2, 2)
 
-    bar_width = 0.3
-    # for legend
-
-    index = range(len(x))
+    ax1.set_xticks(index)
+    ax1.set_xticks(index2, minor=True)
+    ax1.grid(which='major', alpha=0, axis='x')
+    ax1.grid(which='minor', alpha=1, axis='x')
 
     b1 = plt.bar(
-        index,
+        [0.4 + i for i in index],
         packet,
         color="limegreen",
         width=bar_width,
         alpha=alpha,
-        label="Packet Count",
-        edgecolor="g",
+        zorder=3,
+        align='center',
+        label='All Packets'
     )
     b2 = plt.bar(
-        [i + bar_width for i in index],
-        cluster_count,
-        color="b",
+        [0.5 + i+bar_width for i in index],
+        bigcluster_packets,
+        color='darkgreen',
         width=bar_width,
-        alpha=alpha,
-        label="Cluster Count",
+        zorder=3,
+        align='center',
+        label='Largest-cluster Packets'
     )
-
-    for i in range(len(x)):
-        if len(cluster_packets[i]) == 2:
-            b4 = plt.bar(
-                index[i],
-                sum(cluster_packets[i][:2]),
-                color="limegreen",
-                label="Biggest Cluster",
-                width=bar_width,
-                alpha=alpha,
-                edgecolor="black",
-            )
-        b3 = plt.bar(
-            index[i],
-            cluster_packets[i][0],
-            color="darkgreen",
-            label="Remain",
-            width=bar_width,
-            alpha=1,
-            edgecolor="black",
-        )
-    if "b4" in locals():
-        plt.legend(handles=(b1, b2, b3, b4))
-    else:
-        plt.legend(handles=(b1, b2, b3))
-    # plt.yticks(range(max(cluster_count) + 1, ))
+    b3 = plt.bar(
+        [0.8 +i+bar_width for i in index],
+        most_occurrence_signature,
+        color = 'yellow',
+        width=bar_width,
+        zorder=3,
+        align='center',
+        label='Mode Signature Count'
+    )
+    plt.legend(handles=(b1, b2, b3))
+    plt.yticks(range(max(packet) + 1))
     plt.title("Group Cluster Result(sort by cardinality)")
-    plt.xlabel("Cluster Name")
-    plt.ylabel("Count")
-    plt.yscale("log")
-    plt.xticks(
-        np.arange(bar_width / 2, len(x) + bar_width / 2, 1),
-        x,
-        fontsize=8,
-        rotation=25,
-        ha="right",
-        rotation_mode="anchor",
-    )
+    plt.xlabel('Group Name')
+    plt.ylabel('Count')
+    plt.yscale('log')
+    plt.grid()
+    plt.tight_layout()
+    plt.xticks(np.arange(bar_width +0.5  , len(index)*2 + bar_width, 2), x, fontsize = 8)
     plt.savefig(
         os.path.join(result_path, "group_summary_graph.png"),
-        dpi=300,
-        facecolor="#eeeeee",
+        dpi=300, facecolor='#eeeeee',
         transparent=True,
-        bbox_inches="tight",
+        bbox_inches='tight'
     )
     plt.clf()
+
+    main_df = pd.read_csv(os.path.join(result_path, "all_cluster_signatures.csv"))
+    if len(main_df["cluster_key_card"].unique()) == 0:
+        main_df["cluster_key_card"] = 1
+    group_key_df = main_df[["group", "key_card"]]
+
+
     for i in group_key_df["group"].tolist():
         temp_df = main_df[main_df["group"] == i]
         remain = temp_df[temp_df["cluster"] == -1]
@@ -168,7 +141,8 @@ def SummaryGraph(result_path):
         )
         plt.legend()
         #     plt.yticks(range(max(card) + 1))
-        plt.title(temp_df["group"].tolist()[0])
+        if len(temp_df["group"].tolist()) != 0:
+            plt.title(temp_df["group"].tolist()[0])
         plt.xticks(x, clusters)
         plt.savefig(
             os.path.join(result_path, i, "cluster_summary_graph.png"),

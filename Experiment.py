5e595c499bc0229cf02a68f9ca97d4fb69c1e4da
Woohyuk Jang <spectat@kookmin.ac.kr>
2022-09-15 20:30:01 +0900

    remove : parse_config

--- Experiment.py
+++ Experiment.py
@@ -2,26 +2,8 @@ from configparser import ConfigParser
 
 from Main import main
 import os
-from Utils import get_dir
+from Utils import get_dir, parse_config
 
-def parse_config(cfgs):
-    args_dict = dict()
-    args_dict['pcap_dir'] = cfgs["DEFAULT"]['pcap_dir']
-    args_dict['result_path'] = get_dir(cfgs["DEFAULT"]['result_path'], cfgs["DEFAULT"]['result_dir'])
-    args_dict['threshold'] = float(cfgs["DEFAULT"]['threshold'])
-    args_dict['card_th'] = int(cfgs["DEFAULT"]['card_th'])
-    args_dict['group_type'] = cfgs["DEFAULT"]['group']
-    args_dict['israw'] = eval(cfgs["DEFAULT"]['israw'])
-    args_dict['deduplication'] = eval(cfgs["DEFAULT"]['deduplication'])
-    args_dict['iscount'] = eval(cfgs["DEFAULT"]['count'])
-    args_dict['earlystop'] = eval(cfgs["DEFAULT"]['earlystop'])
-    args_dict['vector_size'] = int(cfgs["DEFAULT"]['vector_size'])
-    args_dict['window_size'] = int(cfgs["DEFAULT"]['window_size'])
-    args_dict['hh1_size'] = int(cfgs["DEFAULT"]['hh1_size'])
-    args_dict['hh2_size'] = int(cfgs["DEFAULT"]['hh2_size'])
-    args_dict['ratio'] = float(cfgs["DEFAULT"]['ratio'])
-    
-    return args_dict
 
 def experiment():
     cfgs = ConfigParser()

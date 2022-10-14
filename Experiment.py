40ae4eae0624838a9f9680fa39698c35e8001e40
spectator05 <spectat@kookmin.ac.kr>
2022-10-14 12:11:14 +0900

    refactoring: arg parser, remove if len == 0

--- Experiment.py
+++ Experiment.py
@@ -2,8 +2,13 @@ from configparser import ConfigParser
 import argparse
 
 from Main import main
-
 from Utils import parse_config
+
+ARG_STORE = ["pcap_dir", "regex_path", "cpu_count", "result_path", 'result_dir',\
+        "threshold", "card_th", "group", "vector_size", "window_size", "hh1_size",\
+        "hh2_size", "ratio", "extension"]
+
+ARG_STORE_TRUE = ["israw", "deduplication", "count", "earlystop"]
 
 
 def experiment(args_user):
@@ -11,22 +16,17 @@ def experiment(args_user):
     cfgs.read("config.ini", encoding="UTF-8")
     parsed_args = parse_config(cfgs, args_user)
 
-    if (parsed_args["result_path"]!=0): 
-        main(args=parsed_args)
+    main(args=parsed_args)
+
 
 if __name__ == "__main__":
-    argument_store = ["pcap_dir", "regex_path", "cpu_count", "result_path", 'result_dir',\
-            "threshold", "card_th", "group", "vector_size", "window_size", "hh1_size",\
-            "hh2_size", "ratio", "extension"]
-
-    argument_store_true = ["israw", "deduplication", "count", "earlystop"]
     argparser = argparse.ArgumentParser()
 
-    for i in argument_store:
-        argparser.add_argument(f"-{i}", dest= i, action= "store")
+    for arg in ARG_STORE:
+        argparser.add_argument(f"-{arg}", dest= arg, action= "store")
 
-    for i in argument_store_true:
-        argparser.add_argument(f"-{i}", dest= i, action= "store_true")
+    for arg in ARG_STORE_TRUE:
+        argparser.add_argument(f"-{arg}", dest= arg, action= "store_true")
 
     args_user = vars(argparser.parse_args())
     experiment(args_user)
